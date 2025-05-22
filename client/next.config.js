df1848fbde499ba06c5ba7489f90a430d94082c8
Biplob Das <biplobdasaz1996@gmail.com>
2025-05-22 16:19:45 +0530

    Fixed proxy url

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async rewrites() {
     return [
       { //This is for proxy to flask server from nextjs client, this will fix cors error;
-        source: '/server/:path*',
+        source: '/server_api/:path*',
         destination: 'http://localhost:8000/:path*',
       },
       { // here source /audio/:path* means starting with /audio/ should redirect to destination url given, and replace destination path with source path;
